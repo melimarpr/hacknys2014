fbe3839eaad20665340b3ebb8e74b9c8db73a267
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-04-05 18:52:59 -0400

    Puercada 2

--- foursquarerpgserver/app/controllers/Foursquare.scala
+++ foursquarerpgserver/app/controllers/Foursquare.scala
@@ -7,13 +7,14 @@ import other._
 
 object Foursquare extends Controller {
 		val fsq = new FoursquareApi(FoursquareCredentials.CLIENT_ID, FoursquareCredentials.CLIENT_SECRET, FoursquareCredentials.PUSH_SECRET);
-		fsq.setoAuthToken(FoursquareCredentials.OAUTH_KEY);
 		var values: Map[String, Seq[String]] = _;
 	def redirectGet(code: String) = Action {
-//	      request => values = request.body.asFormUrlEncoded.get;
     println(code)
-    	
-		Redirect(fsq.authenticateCode(code).toString);
+    	fsq.authenticateCode(code)
+		Ok("{\"token\":" +fsq.getOAuthToken() +  "}");
+	}
+	def some = Action {
+	  Redirect(fsq.getAuthenticationUrl());
 	}
 		def redirectPost = Action {
 //	      request => values = request.body.asFormUrlEncoded.get;
