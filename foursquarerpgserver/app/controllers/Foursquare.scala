ad7766974839cf5e1deb824b10b9f5020c26f9ed
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-04-05 17:53:39 -0400

    Cheating to view payload.

--- foursquarerpgserver/app/controllers/Foursquare.scala
+++ foursquarerpgserver/app/controllers/Foursquare.scala
@@ -8,12 +8,15 @@ import other._
 object Foursquare extends Controller {
 		val fsq = new FoursquareApi(FoursquareCredentials.CLIENT_ID, FoursquareCredentials.CLIENT_SECRET, FoursquareCredentials.PUSH_SECRET);
 		fsq.setoAuthToken(FoursquareCredentials.OAUTH_KEY);
-
+		var values: Map[String, Seq[String]] = _;
 	def redirect = Action {
-	  
+	      request => values = request.body.asFormUrlEncoded.get;
+    println(values)
 		Ok("This is /redirect");
 	}
 	def handlePush = Action {
+	  	      request => values = request.body.asFormUrlEncoded.get;
+    println(values)
 		Ok("This is /handle_push.");
 	}
 }
