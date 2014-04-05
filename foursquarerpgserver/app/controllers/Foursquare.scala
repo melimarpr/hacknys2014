e45f12802f200eb66fbba8a433292654dc9f523d
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-04-05 18:58:16 -0400

    Moar porkiness.

--- foursquarerpgserver/app/controllers/Foursquare.scala
+++ foursquarerpgserver/app/controllers/Foursquare.scala
@@ -1,17 +1,18 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
 import fi.foyt.foursquare.api.FoursquareApi
 import other._
+import play.api._
+import play.api.mvc._
 
 object Foursquare extends Controller {
 		val fsq = new FoursquareApi(FoursquareCredentials.CLIENT_ID, FoursquareCredentials.CLIENT_SECRET, FoursquareCredentials.PUSH_SECRET);
 		var values: Map[String, Seq[String]] = _;
+		
 	def redirectGet(code: String) = Action {
     println(code)
     	fsq.authenticateCode(code)
-		Ok("{\"token\":" +fsq.getOAuthToken() +  "}");
+		Ok("{\"token\":" +fsq.getOAuthToken() +  "}").as("application/json");
 	}
 	def some = Action {
 	  Redirect(fsq.getAuthenticationUrl());
