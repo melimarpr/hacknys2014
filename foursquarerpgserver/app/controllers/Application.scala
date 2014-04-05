dfed259041ab96d8caa9702079c882aa1c231ab3
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-04-05 16:52:31 -0400

    Initial commit.

---
+++ foursquarerpgserver/app/controllers/Application.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    
+    Ok(views.html.index("Your new application is ready."))
+  }
+  def redirect = Action {
+    Ok("");
+    }
+  def handlePush = Action {
+    Ok("");
+  }
+  
+
+}
