153daa4e2baf2b9d8e55398d20a9d643337b0129
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-04-06 06:56:23 -0400

    afdssdfsd

--- foursquarerpgserver/app/controllers/Foursquare.scala
+++ foursquarerpgserver/app/controllers/Foursquare.scala
@@ -71,7 +71,7 @@ object Foursquare extends Controller {
 		}
 		val monsterIntId = enemy.id;
 		val enemyName = enemy.name;
-		sendPush(enemyName,monsterIntId, userId.as[Int]);
+		sendPush(enemyName,monsterIntId, userId.toString.trim().toInt);
 		Ok(views.html.index("Success!"))
 	}
 
